03f46e3c5a608f5c0191020b7c14da52e799c6d8
Alan Fachini <alfakini@gmail.com>
2019-12-09 22:42:02 -0300

    Config Devise routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root '/', as: :authenticated_root
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
